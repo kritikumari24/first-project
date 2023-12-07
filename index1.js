50a9214fe5f441526698148821c9237b7b313b6d
kritikumari24 <kumarikriti213@gmail.com>
2023-12-07 21:03:32 +0530

    mongodb

---
+++ index1.js
@@ -0,0 +1,13 @@
+const express =require('express');
+const app=express();
+
+app.get('',(req,res)=>{
+    res.send('hello,this is home page');
+});
+app.get('/about',(req,res)=>{
+    res.send('hello,this is about page');
+});
+app.get('/help',(req,res)=>{
+    res.send('hello,this is Help page');
+});
+app.listen(5000);
---
+++ index2.js
@@ -0,0 +1,16 @@
+const{MongoClient} =require('mongodb');
+const url = 'mongodb://localhost:27017';
+const database='ecom'
+const client=new MongoClient(url);
+
+
+async function getData()
+{
+    let result=await client.connect();
+    let db=result.db(database);
+    let collection =db.collection('products');
+    let response =await collection.find({name:'m-40'}).toArray();
+    console.log(response);
+}
+
+getData();
